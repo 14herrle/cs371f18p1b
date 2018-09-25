30558eba11feb8525a31f50ad3b8ef30642000f6
Alejandro Herrle <aherrle@luc.edu>
2018-09-25 11:12:00 -0500

    RIP Bubble Sort. Your contribution will not be forgotten (Quicksort Implemented in place of bubble sort)
    [Alex To Scala]: WHY CAN'T YOU JUST BE NORMAL
    
    [Scala]: *SCREAMS*

--- src/test/Scala/WordCloudTest.scala
+++ src/test/Scala/WordCloudTest.scala
@@ -1,21 +1,35 @@
-package src
+package com.topwords
 
-import org.scalatest.funsuite
+import org.scalatest._
 
+class WordCloudTest extends WordSpec {
 
-
-class WordCloudTest extends FunSuite {
-
-  test("A new word cloud should be empty") {
-    assert((new WordCloud).getSize() == 0)
+  /*test("A new word cloud should be empty") {
+    assert((new com.topwords.WordCloud).getSize() == 0)
   }
 
   test("A new word queue should be empty") {
-    assert((new WordQueue).getQueueSize() == 0)
+    assert((new com.topwords.WordQueue).getQueueSize() == 0)
   }
 
-  test("the addWord method of WordQueue adds a new word to the queue") {
 
+  test("the addWord method of com.topwords.WordQueue adds a new word to the queue") {
+    val tQ = new com.topwords.WordQueue
+    tQ.add("word")
+    assert(tQ.getQueue.equals(new Queue.empty[String] += "word"))
+  }
+  */
+
+  val q = new WordQueue
+  val c = new WordCloud
+
+  "The the function getQueueSize" when {
+    "given a new wordQueue" should {
+      "be size 0" in {
+        val result: Int = q.getQueueSize
+        assert(result == 0)
+      }
+    }
   }
 
 }
